b4e27d50a1806cfbcdd58e9b5ae2888b01bbb6f1
dmelgarm <trucutu.dm@gmail.com>
2014-04-25 16:31:41 -0600

    ad

--- figures/iwt009.py
+++ figures/iwt009.py
@@ -2,11 +2,17 @@ from scipy.io import loadmat
 from scipy.integrate import cumtrapz
 import matplotlib.pyplot as pl
 
-a= a=loadmat('/Users/dmelgarm/Documents/dissertation/figures/IWT009.e.mat')['a']
+a=loadmat('/Users/dmelgarm/Documents/dissertation/figures/IWT009.e.mat')['a']
 t=a[:,0]
 a=a[:,1]
 v=cumtrapz(a,t,initial=0)
 d=cumtrapz(v,t,initial=0)
+
+g=loadmat('/Users/dmelgarm/Documents/dissertation/figures/IWT009.egps.mat')['g']
+tg=g[:,0]
+g=g[:,1]
+
+bl=loadmat('/Users/dmelgarm/Documents/dissertation/figures/IWT009.bl.mat')['bl']
 
 pl.rc('font', family='serif')
 pl.close("all")
@@ -28,4 +34,17 @@ pl.grid(which='both')
 pl.xlim(7,300)
 pl.xlabel('Seconds after origin time')
 pl.savefig('/Users/dmelgarm/Documents/dissertation/figures/iwt009.eps')
+
+pl.figure()
+pl.plot(t,bl[:,1],t,bl[:,2],t,bl[:,3],t,bl[:,4],t,bl[:,5])
+pl.plot(tg,g,'k')
+pl.xlim(7,250)
+pl.ylim(-1,4.5)
+pl.grid(which='both')
+pl.xlabel('Seconds after origin time')
+pl.ylabel('East displacement $(m)$')
+pl.legend(['$t_1=75s$','$t_1=100s$','$t_1=125s$','$t_1=150s$','20s highpass','GPS'],loc=2)
+pl.savefig('/Users/dmelgarm/Documents/dissertation/figures/iwt009bl.eps')
 pl.show()
+
+
